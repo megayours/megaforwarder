6f44a75c3dbe62d3b2b33b7eddbfe9b84077622d
Viktor Plane <viktor@planevin.se>
2025-03-24 10:51:14 +0100

    feat: fix logging

--- src/util/create-provider.ts
+++ src/util/create-provider.ts
@@ -1,6 +1,7 @@
 import { AnkrProvider, QuickNodeProvider } from "ethers";
 import { AlchemyProvider, InfuraProvider } from "ethers";
 import type { Rpc } from "../core/types/config/Rpc";
+import { logger } from "./monitoring";
 
 export const createRandomProvider = (rpcs: Rpc[]) => {
   const randomRpc = rpcs[Math.floor(Math.random() * rpcs.length)];
@@ -12,12 +13,16 @@ export const createRandomProvider = (rpcs: Rpc[]) => {
   const type = randomRpc.type;
 
   if (type === "alchemy") {
+    logger.info(`Creating Alchemy provider with chain: ${chain} and token: ${apiKey}`);
     return { provider: new AlchemyProvider(chain, apiKey), token: apiKey };
   } else if (type === "infura") {
+    logger.info(`Creating Infura provider with chain: ${chain} and token: ${apiKey}`);
     return { provider: new InfuraProvider(chain, apiKey), token: apiKey };
   } else if (type === "quicknode" || type === "quiknode") {
+    logger.info(`Creating QuickNode provider with chain: ${chain} and token: ${apiKey}`);
     return { provider: new QuickNodeProvider(chain, apiKey), token: apiKey };
   } else if (type === "ankr") {
+    logger.info(`Creating Ankr provider with chain: ${chain} and token: ${apiKey}`);
     return { provider: new AnkrProvider(chain, apiKey), token: apiKey };
   }
 
