c6e37ba22b7149b6071d62c07773332645789269
Viktor Plane <viktor@planevin.se>
2025-03-24 10:18:56 +0100

    feat: add ankr support

--- src/util/create-provider.ts
+++ src/util/create-provider.ts
@@ -1,4 +1,4 @@
-import { QuickNodeProvider } from "ethers";
+import { AnkrProvider, QuickNodeProvider } from "ethers";
 import { AlchemyProvider, InfuraProvider } from "ethers";
 import type { Rpc } from "../core/types/config/Rpc";
 
@@ -10,12 +10,15 @@ export const createRandomProvider = (rpcs: Rpc[]) => {
   const apiKey = randomRpc.apiKey;
   const chain = randomRpc.chain;
   const type = randomRpc.type;
+
   if (type === "alchemy") {
     return new AlchemyProvider(chain, apiKey);
   } else if (type === "infura") {
     return new InfuraProvider(chain, apiKey);
   } else if (type === "quicknode" || type === "quiknode") {
     return new QuickNodeProvider(chain, apiKey);
+  } else if (type === "ankr") {
+    return new AnkrProvider(chain, apiKey);
   }
 
   throw new Error(`Unsupported RPC type: ${type}`);
